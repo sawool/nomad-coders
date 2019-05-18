d785fd8f4bbe7f8275602a3cdd02de5bf3e344fc
sawool <sawool82@gmail.com>
2019-05-18 14:13:41 +0900

    Updates

---
+++ precache-manifest.b8278eec8cac8f17e24ef316cee8349d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673f37f344e49d17c5f6532fd22a1619",
+    "url": "/nomadcoders-movie_app/index.html"
+  },
+  {
+    "revision": "9a264a804fa3c7cadeba",
+    "url": "/nomadcoders-movie_app/static/css/main.188078a7.chunk.css"
+  },
+  {
+    "revision": "ad63ab08a2a8e5504836",
+    "url": "/nomadcoders-movie_app/static/js/2.7b834e7d.chunk.js"
+  },
+  {
+    "revision": "9a264a804fa3c7cadeba",
+    "url": "/nomadcoders-movie_app/static/js/main.b95b1b30.chunk.js"
+  },
+  {
+    "revision": "bbe3d497af179657d78e",
+    "url": "/nomadcoders-movie_app/static/js/runtime~main.834ad94e.js"
+  }
+]);
